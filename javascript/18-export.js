1711a03c7e3a9046d8d92e60c827e49cf63fa5ca
mobinberenjabadi <you@example.com>
2024-03-15 01:51:49 +0330

    new

---
+++ javascript/18-export.js
@@ -0,0 +1,8 @@
+export function sum2num(num1,num2){
+    console.log(num1 + num2)
+}
+export function min2num(num1,num2){
+    console.log(num1 - num2)
+}
+
+// export { min2num , sum2num } ya intori ya toye function
---
+++ javascript/18-import.js
@@ -0,0 +1,4 @@
+import {min2num,sum2num} from './18-export.js'
+
+min2num(40,30)
+sum2num(20,20)
